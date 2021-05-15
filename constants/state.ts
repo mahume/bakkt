f0adeac8462cc54165f3ae65fe6ebaed824e5b11
Mike Hume <michael.a.hume@pm.me>
2021-05-15 18:29:38 -0400

    feat: add basic files and dummy field for initial state object to be used with context

---
+++ constants/state.ts
@@ -0,0 +1,5 @@
+import {AppState} from "../typescript/interfaces/state";
+
+export const INITIAL_APP_STATE: AppState = {
+    test: "",
+};
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,2 @@
+export * as interfaces from "./interfaces/state";
 export * as types from "./types";
