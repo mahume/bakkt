8262d9f7feeebeb0c68057c5a7479d73237d5f09
Mike Hume <michael.a.hume@pm.me>
2021-05-15 19:44:05 -0400

    feat: disable click when not in gameplay

--- components/GameBoard/GridSpace.tsx
+++ components/GameBoard/GridSpace.tsx
@@ -2,18 +2,23 @@ import React, {FC} from "react";
 import {Text, View} from "../Themed";
 import {Column, Row} from "../../typescript/types/board";
 import {Pressable, StyleSheet} from "react-native";
+import {useAppContext} from "../../context";
 
 interface Props {
     row: Row;
     column: Column;
 }
 
-const GridSpace: FC<Props> = ({ row , column}) => {
+const GridSpace: FC<Props> = ({row, column}) => {
+    const {state} = useAppContext();
+    const isSpaceDisabled = !state.isGameInProgress
+
     const handleOnPress = (row: string, column: string) => alert(row + column);
 
     return (
         <View style={styles.space}>
-            <Pressable style={styles.button} onPress={() => handleOnPress(row, column)}>
+            <Pressable style={styles.button} disabled={isSpaceDisabled}
+                       onPress={() => handleOnPress(row, column)}>
                 <Text style={styles.mark}>{row + column}</Text>
             </Pressable>
         </View>
