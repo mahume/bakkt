6bfb49b6eba9f030777efa930804b1d333fcd163
Mike Hume <michael.a.hume@pm.me>
2021-05-15 19:32:13 -0400

    feat: RoundAction.tsx onPress

--- components/GameActions/RoundAction.tsx
+++ components/GameActions/RoundAction.tsx
@@ -1,16 +1,20 @@
 import React, {FC} from "react";
-import {Text, View} from "../Themed";
-import {StyleSheet} from "react-native";
+import {View} from "../Themed";
+import {Button, StyleSheet} from "react-native";
+import {useAppContext} from "../../context";
 
 const RoundAction: FC = () => {
+    const { state } = useAppContext();
+
+    const buttonText = state.isGameInProgress ? "Restart Round" : "Start Round";
+
+    const handleRoundOnPress = () => alert("play round");
+
     return (
         <View>
-            <Text>Round Action</Text>
+            <Button onPress={handleRoundOnPress} title={buttonText}/>
         </View>
     )
 };
-
-const styles = StyleSheet.create({});
-
 
 export default RoundAction;
