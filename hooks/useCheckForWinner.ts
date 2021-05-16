0cba1d42c60c94da9fc10dc239ac8d3d4fe59be7
Mike Hume <michael.a.hume@pm.me>
2021-05-15 21:04:35 -0400

    feat: custom hook checks for horizontal wins

--- hooks/useCheckForWinner.ts
+++ hooks/useCheckForWinner.ts
@@ -1,20 +1,35 @@
 import {useAppContext} from "../context";
 import {useEffect} from "react";
+import {Player, Row} from "../typescript/types/board";
 
 
 const useCheckForWinner = () => {
-    const { state, dispatch } = useAppContext();
+    const { state } = useAppContext();
 
     // TODO:
-    // Watch Grid changes
+    // 2. Quarterback fn
+    function checkWinFor(player: Player) {
+        checkHorizontalRows("A", player);
+        checkHorizontalRows("B", player);
+        checkHorizontalRows("C", player);
+    }
+
     // Check for horizontal win
+    function checkHorizontalRows(row: Row, player: Player) {
+        const gridRow = Object.keys(state.grid).filter(key => key.startsWith(row));
+        const isWinner = gridRow.every(column => state.grid[column] === player);
+        console.log(isWinner);
+    }
     // Check for vertical win
     // Check for diagonal win
     // Check for draw
     // If there is a winner, take appropriate actions
 
 
+    // 1. Watch Grid changes
     useEffect(() => {
+        checkWinFor("X");
+        checkWinFor("O");
     }, [state.grid])
 }
 
