cc8a29dc9e51023bfeab44d1a8fe1d87571b9fe5
Mike Hume <michael.a.hume@pm.me>
2021-05-15 20:09:23 -0400

    feat: tie up MatchAction.tsx to fire INITIALIZE_GAME action

--- components/GameActions/MatchAction.tsx
+++ components/GameActions/MatchAction.tsx
@@ -1,9 +1,16 @@
 import React, {FC} from "react";
 import {Button} from "react-native";
 import {View} from "../Themed";
+import {useAppContext} from "../../context";
 
 const MatchAction: FC = () => {
-    const handleGameResetOnPress = () => alert("Reset");
+    const { dispatch } = useAppContext();
+
+    const handleGameResetOnPress = () => {
+        dispatch({
+            type: "INITIALIZE_GAME",
+        })
+    }
 
     return (
         <View>
